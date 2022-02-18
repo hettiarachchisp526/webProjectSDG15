8d3dad4a5e284e0d0defd83cfc755b96a8e745d7
Bhagya-Senevirathna <87971735+Bhagya-Senevirathna@users.noreply.github.com>
2022-02-18 22:13:47 +0530

    modifyjs

--- JS/user-create-account.js
+++ JS/user-create-account.js
@@ -1,48 +1,51 @@
+const imgDiv= document.querySelector('.profile-pic-div');
+const img= document.querySelector('#prfphoto');
+const file= document.querySelector('#file');
+const uploadButton= document.querySelector('#uploadButton');
 
-function validate(){
-         if(document.frm1.fname.value.length<1){
-             alert("Please enter First name");
+        file.addEventListener('change',function(){
+        const choosedFile= this.files[0];
+
+          if(choosedFile){
+                    const reader = new FileReader();
+                    
+                    reader.addEventListener('load',function()
+                    { 
+                         img.setAttribute('src',reader.result);
+                     });
+
+                 reader.readAsDataURL(choosedFile);
+               }
+});
+
+
+    function validateform(){
+
+        if(document.frm1.fname.value.length<1){
+            window.alert("Please Input First Name");
               }
+
         if(document.frm1.lname.value.length<1){
-             alert("Please enter Last name");
-              }
-              if(document.frm1.email.value.length<1){
-             alert("Please enter a email");
-              }
-              var a=document.frm1.email.value.indexOf("@");
-	          var b=document.frm1.email.value.lastIndexOf(".");
-	        if(a<1 || b-a<2 || b+2>=email.length)
-	         {
-                 alert("Invalid Email address");
-	         }
+            window.alert("Please Input Last Name");
+             }
+           
+        if(document.frm1.pass.value.length<9){
+            window.alert("Your password should have at least 8 characters");
+             } 
+             //email validation
+             var a=document.frm1.email.value.indexOf("@");
+             var b=document.frm1.email.value.lastIndexOf(".");
+             if(a<1 || b-a<2 || b+2>=email.length)
+             {window.alert("Invalid Email address");
+             }
+          
+            //password confirmation
+            var password = document.getElementById("passid").value;
+        var confirmPassword = document.getElementById("cpassid").value;
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+	
+    }
 
-         if(document.frm1.pass.value.length<1){
-             alert("Please enter Password");
-              }
 }
-
-    
-    const imgDiv= document.querySelector('.profile-pic-div');
-    const img= document.querySelector('#photo');
-    const file= document.querySelector('#file');
-    const uploadButton= document.querySelector('#uploadButton');
-    
-    
-    //image showing functionality when we choose an image to upload
-    //when we choose a photo to upload
-    
-    file.addEventListener('change',function(){
-             const choosedFile= this.files[0];
-    
-               if(choosedFile){
-                         const reader = new FileReader();
-                         
-                         reader.addEventListener('load',function()
-                         { 
-                              img.setAttribute('src',reader.result);
-                          });
-    
-                      reader.readAsDataURL(choosedFile);
-                    }
-    });
     
