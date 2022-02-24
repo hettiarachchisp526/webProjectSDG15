120c5caa51862bbb34bdf87a3f3ed1f940e6a47d
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2022-02-24 10:24:24 +0530

    home link 1

--- JS/homejs.js
+++ JS/homejs.js
@@ -138,4 +138,14 @@
       }
     });
   
-  })()
+  })();
+  function profile() {
+    let confirmAction = confirm("Do you use this as a normal user-press=OK,  or as a resercher press=cancel");
+    if (confirmAction) {
+      window.open("user-create-account-reasearch.html");
+    } 
+    
+    else {
+      window.open("user-create-account.html");
+    }
+  }
